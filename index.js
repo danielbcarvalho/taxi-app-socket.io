869bab6152cda81d323ac0a551ee78ffb03ba495
daniel-07 <danielborgesdecarvalho@gmail.com>
2021-01-03 16:21:43 -0300

    add socket.io for passenger and driver solicitations

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = 3000;
 
+let taxiSocket = null;
+
 io.on('connection', (socket) => {
   console.log('L', 'a user connected =D');
-  socket.on('chat message', (msg) => {
-    console.log('L', msg);
-    io.emit('chat message', msg);
+  socket.on('taxiRequest', (routeResponse) => {
+    console.log("Someone is looking for a taxi!", )
+    if (taxiSocket != null) {
+      taxiSocket.emit('taxiRequest', routeResponse)
+    }
   });
+
+  socket.on('lookingForPassengers', () => {
+    console.log("Looking for passengers!", )
+    taxiSocket = socket;
+  })
 });
 
 server.listen(port, () => console.log('L', 'server running...'));
